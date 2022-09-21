bba5e22c8b1ee5536bd8b3a8834b96d4deb72e6c
87marsan <111994282+87marsan@users.noreply.github.com>
2022-09-21 14:07:30 +0200

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en veckodag: ");
+            string input = Console.ReadLine().ToLower();
+            string dayName = input.Substring(0,1);
+            dayName = dayName.ToUpper();
+            dayName += input.Substring(1);
+
+            object? objThing = null;
+            Enum.TryParse(typeof(Veckodagar), dayName, out objThing);
+            Veckodagar miniX = (Veckodagar)objThing;
+
+            Console.WriteLine((int)(Veckodagar)miniX);
+
+        }
+
+        
+    }
+    public enum Veckodagar
+    {
+        Måndag = 1,
+        Tisdag = 2,
+        Onsdag,
+        Tortsdag,
+        Fredag,
+        Lördag,
+        Söndag
+
+    }
+}
